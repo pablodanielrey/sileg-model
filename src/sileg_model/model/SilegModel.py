87add0f6f6d64fd361b402278b94d395f2631d6a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-16 22:26:44 -0300

    movi la funcionalidad a una funcion aparte para no generar basura dentro del modelo del sistema

--- src/sileg_model/model/SilegModel.py
+++ src/sileg_model/model/SilegModel.py
@@ -3,36 +3,5 @@ from .entities.Function import Function, FunctionTypes
 
 class SilegModel:
 
-    @staticmethod
-    def _authority_functions():
-        return ['Decano', 'Vicedecano', 'Secretario', 'Prosecretario']
-
-    @staticmethod
-    def _teacher_functions():
-        functions = ['Adjunto', 'Asociado', 'Ayudante Alumno', 'Ayudante Diplomado', 'Jefe de Auxiliares Docentes', 'Jefe de Trabajos Prácticos']
-        dedications = ['Exclusiva', 'Semi Dedicación', 'Semi Exclusiva', 'Simple', 'Tiempo Completo']
-        caracters = ['Ad honorem', 'Interino', 'Ordinario', 'Suplente']
-        rf = []
-        for f in functions:
-            for d in dedications:
-                for c in caracters:
-                    rf.append(f'{f} - {d} - {c}')
-        return rf
-
-    def _insert_functions(self, session):
-        for f in SilegModel._authority_functions():
-            if session.query(Function.id).filter(Function.name == f).count() <= 0:
-                ff = Function()
-                ff.name = f
-                ff.type = FunctionTypes.AUTORIDAD
-                session.add(ff)
-
-        for f in SilegModel._teacher_functions():
-            if session.query(Function.id).filter(Function.name == f).count() <= 0:
-                ff = Function()
-                ff.name = f
-                ff.type = FunctionTypes.DOCENTE
-                session.add(ff)
-
     def get_functions(self, session):
         return session.query(Function).all()
--- src/sileg_model/model/__main__.py
+++ src/sileg_model/model/__main__.py
@@ -2,10 +2,43 @@
 from sileg_model.model import create_tables
 create_tables()
 
-from sileg_model.model import open_session
-from sileg_model.model.SilegModel import SilegModel
+def insert_model_data():
+    """
+        Se generan los datos para inicializar la base de datos.
+    """
 
-with open_session() as ss:
-    s = SilegModel()
-    s._insert_functions(ss)
-    ss.commit()
+    from sileg_model.model import open_session
+    from sileg_model.model.entities.Function import Function, FunctionTypes
+
+    def _authority_functions():
+        return ['Decano', 'Vicedecano', 'Secretario', 'Prosecretario']
+
+    def _teacher_functions():
+        functions = ['Adjunto', 'Asociado', 'Ayudante Alumno', 'Ayudante Diplomado', 'Jefe de Auxiliares Docentes', 'Jefe de Trabajos Prácticos']
+        dedications = ['Exclusiva', 'Semi Dedicación', 'Semi Exclusiva', 'Simple', 'Tiempo Completo']
+        caracters = ['Ad honorem', 'Interino', 'Ordinario', 'Suplente']
+        rf = []
+        for f in functions:
+            for d in dedications:
+                for c in caracters:
+                    rf.append(f'{f} - {d} - {c}')
+        return rf
+
+    def _insert_functions(session):
+        for f in _authority_functions():
+            if session.query(Function.id).filter(Function.name == f).count() <= 0:
+                ff = Function()
+                ff.name = f
+                ff.type = FunctionTypes.AUTORIDAD
+                session.add(ff)
+
+        for f in _teacher_functions():
+            if session.query(Function.id).filter(Function.name == f).count() <= 0:
+                ff = Function()
+                ff.name = f
+                ff.type = FunctionTypes.DOCENTE
+                session.add(ff)
+
+    with open_session() as ss:
+        _insert_functions(ss)
+        ss.commit()
