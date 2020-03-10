a980611e66205a0ff4a9f3bb3958614f6ba184ad
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-03-10 11:50:08 -0300

    corregimos el problema de generar el engine cuando se importa el modelo y no cuando se obtiene la sesion

--- src/sileg_model/model/__init__.py
+++ src/sileg_model/model/__init__.py
@@ -3,18 +3,16 @@ import contextlib
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
+@contextlib.contextmanager
+def open_session():
+    engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
         os.environ['SILEG_DB_USER'],
         os.environ['SILEG_DB_PASSWORD'],
         os.environ['SILEG_DB_HOST'],
         os.environ['SILEG_DB_PORT'],
         os.environ['SILEG_DB_NAME']
     ))
-
-Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-@contextlib.contextmanager
-def open_session():
+    Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
     session = Session()
     try:
         yield session
