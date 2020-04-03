cc49fa6be53476a48b80b95d1a6eea352e70b6ed
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-04-03 17:09:14 -0300

    fixes: #2

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sileg-model',
-          version='0.1.34',
+          version='0.1.35',
           description='proyecto de las designaciones de docentes/no-docentes de la facultad',
           url='https://github.com/pablodanielrey/sileg-model',
           author='Desarrollo DiTeSi, FCE',
